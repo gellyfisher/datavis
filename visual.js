ad458fba4e473c27ade7a238500fc5e895c17816
gellyfisher <gelatine1@hotmail.be>
2018-04-21 13:23:47 +0200

    testing API

--- visual.js
+++ visual.js
@@ -4,8 +4,9 @@ function getData() {
 	var dataTxt=$.ajax({
 	    type: "GET", 
 	    url: "https://min-api.cryptocompare.com/data/histoday?fsym=ETH&tsym=BTC&limit=30&aggregate=1&toTs=1452680400&extraParams=datavis"
-	})
-	var data = JSON.parse(dataTxt.responseText)
+	});
+	console.log(dataTxt.responseText);
+	var data = JSON.parse(dataTxt.responseText);
 	console.log(data);
 }
 
