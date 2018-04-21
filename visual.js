ae4b70a1091fc19ab1edec5bbd7f6ac9ee9d4f20
gellyfisher <gelatine1@hotmail.be>
2018-04-21 13:19:42 +0200

    Update visual.js

--- visual.js
+++ visual.js
@@ -1,7 +1,7 @@
 
 
 function getData() {
-	$.ajax({
+	data=$.ajax({
 	    type: "GET", 
 	    url: "https://min-api.cryptocompare.com/data/histoday?fsym=ETH&tsym=BTC&limit=30&aggregate=1&toTs=1452680400&extraParams=datavis"
 	})
