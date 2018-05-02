74f690bccb37f3b2b608357ac88c9c83ea2ac27f
raddecke <ruben.dedecker@ugent.be>
2018-05-02 18:10:17 +0200

    changes

--- jummyjummydonuts.js
+++ jummyjummydonuts.js
@@ -1,124 +1,123 @@
-var data1 = [
-    {indx: 0, name: "one", value: 130},
-    {indx: 1, name: "two", value:  80},
-    {indx: 2, name: "three", value:  35},
-    {indx: 3, name: "four", value:  175},
-    {indx: 4, name: "five", value:  50}
+var data1 = [{
+    indx: 0,
+    name: "one",
+    value: 130
+  },
+  {
+    indx: 1,
+    name: "two",
+    value: 80
+  },
+  {
+    indx: 2,
+    name: "three",
+    value: 35
+  },
+  {
+    indx: 3,
+    name: "four",
+    value: 175
+  },
+  {
+    indx: 4,
+    name: "five",
+    value: 50
+  }
 ];
 
-var data2 = [
-    {indx: 0, name: "one", value: 20},
-    {indx: 1, name: "two", value:  46},
-    {indx: 2, name: "three", value:  15},
-    {indx: 3, name: "four", value:  80},
-    {indx: 4, name: "five", value:  43}
+var data2 = [{
+    indx: 0,
+    name: "one",
+    value: 20
+  },
+  {
+    indx: 1,
+    name: "two",
+    value: 46
+  },
+  {
+    indx: 2,
+    name: "three",
+    value: 15
+  },
+  {
+    indx: 3,
+    name: "four",
+    value: 80
+  },
+  {
+    indx: 4,
+    name: "five",
+    value: 43
+  }
 ];
 
 var currentdata = data1
+var path;
+var pie;
 
 function setUpDonutChart() {
 
-    margin = {top: 20, right: 20, bottom: 20, left: 20};
-    donut_width = 400 - margin.left - margin.right;
-    donut_height = donut_width - margin.top - margin.bottom;
-    outerRadius = Math.min(donut_width, donut_height) / 3;
-    innerRadius = Math.min(donut_width, donut_height) / 4;
-    extend_pixels = 8;
+  margin = {
+    top: 20,
+    right: 20,
+    bottom: 20,
+    left: 20
+  };
+  donut_width = 400 - margin.left - margin.right;
+  donut_height = donut_width - margin.top - margin.bottom;
+  outerRadius = Math.min(donut_width, donut_height) / 3;
+  innerRadius = Math.min(donut_width, donut_height) / 4;
+  extend_pixels = 8;
 
-
-   
-
-    arc = d3.arc()
-    .padRadius(outerRadius)
+  arc = d3.arc()
+    .outerRadius(outerRadius)
     .innerRadius(innerRadius);
 
-
-    pie = d3.pie()
+  pie = d3.pie()
     .padAngle(0.02)
-    .startAngle(1.1*Math.PI)
-    .endAngle(3.1*Math.PI)
-    .value(function(d) { return d.value; });
-    
+    .startAngle(1.1 * Math.PI)
+    .endAngle(3.1 * Math.PI)
+    .value(function(d) {
+      return d.value;
+    });
 
-    donut_chart = d3.select("#graph2").append('svg')
-        .attr("width", donut_width + margin.left + margin.right)
-        .attr("height", donut_height + margin.top + margin.bottom)
-        .append("g")
-        .attr("id", "donutChart")
-        .attr("transform", "translate(" + ((donut_width/2)+margin.left) + "," +
-                                      ((donut_height/2)+margin.top) + ")");
-        
+  donut_chart = d3.select("#graph2").append('svg')
+    .attr("width", donut_width + margin.left + margin.right)
+    .attr("height", donut_height + margin.top + margin.bottom)
+    .append("g")
+    .attr("id", "donutChart")
+    .attr("transform", "translate(" + ((donut_width / 2) + margin.left) + "," +
+      ((donut_height / 2) + margin.top) + ")");
 
-	donut_chart.append("text")
-    .attr("x", - donut_width / 2)
-    .attr("y", - height / 2)
+
+  donut_chart.append("text")
+    .attr("x", -donut_width / 2)
+    .attr("y", -height / 2)
     .text("change_testing_dataset")
     .style("cursor", "hand")
     .on("click", swap_dataset);
 
+}
+
+function drawDonutChart(data) {
     colors = ["#8b0000", "#f091e7", "#f8ab52", "#ADD6FB", "#28e57a"];
-    
     donut_chart.selectAll("path")
-    .data(pie(currentdata))
-    .enter().append("path")
-    .style("fill", function(d) { return colors[d.data.indx]; })
-    .each(function(d) { console.log(d); d.outerRadius = outerRadius - extend_pixels; })
-    .attr("d", arc)
-    .on("mouseover", arcTween_extend(outerRadius, 0))
-    .on("mouseout", arcTween_extend(outerRadius - extend_pixels, 150))
-    
-    updateDonutChart(currentdata)
-
-
-}
-
-function updateDonutChart(passed_data) {
-
-
-    var path = donut_chart.selectAll("path")
-        .data(pie(data1))
-        .enter()
-        .append("path");
-
-    path.transition()
-        .duration(500)
-        .attr("d", arc)
-        .each(function(d) { this._current = d; }); // store the initial angles
-
-    
-    console.log("updating donut")
-    path.data(pie(passed_data));
-    path.transition().duration(750).attrTween("d", arcTween); // redraw the arcs
-    
-}
-
-function swap_dataset() {
-    console.log("swapping")
-    if (currentdata == data1) {
-        currentdata = data2;
-    } else {
-        currentdata = data1;
-    }
-
-    console.log(currentdata)
-    updateDonutChart(currentdata)
+      .data(pie(currentdata))
+      .enter().append("path")
+      .style("fill", function(d) {
+        return colors[d.data.indx];
+      })
+      .attr("d", arc)
 }
 
 function arcTween(a) {
-    var i = d3.interpolate(this._current, a);
-    this._current = i(0);
-    return function(t) {
-      return arc(i(t));
-    };
-  }
-
-
-function arcTween_extend(outerRadius, delay) {
-    return function() {
-        d3.select(this).transition().delay(delay).attrTween("d", function(d) {
-        var i = d3.interpolate(d.outerRadius, outerRadius);
-        return function(t) { d.outerRadius = i(t); return arc(d); };
-        });
-    };
+  var i = d3.interpolate(this._current, a);
+  this._current = i(0);
+  return function(t) {
+    return arc(i(t));
+  };
 }
+
+
 setUpDonutChart()
--- visual.js
+++ visual.js
@@ -14,26 +14,26 @@ let graphType="candle";
 
 $(document).ready(function() {
 	$("#graph-type").val(graphType); //set initial value of the dropdown
-	
+
 	$("#graph-type").change(function () {
 		graphType = this.value; //the selected value
 		$("#graph").empty();
 		setUp();
 		requestData();
 	});
-	
+
     setUp();
 	requestData();
 });
 
-function requestData(currency="BTC") {	
+function requestData(currency="BTC") {
 	let url="https://min-api.cryptocompare.com/data/histo";
 	let amount;
-	
+
 	let timeInBetween=(end-start)/(60*1000*NUM_POINTS)
 	let endTimeStamp=Math.floor(end.getTime() / 1000);
 	endTimeStamp-=endTimeStamp%3600 //fix it for hourly data
-		
+
 	if (timeInBetween>=24*60) {
 		url+="day";
 		amount=Math.round(timeInBetween/1440);
@@ -43,159 +43,59 @@ function requestData(currency="BTC") {
 	} else {
 		throw "invalid timeInBetween for the data request";
 	}
-	
+
 	params={
-	    fsym: currency, 
+	    fsym: currency,
 	    tsym: "EUR",
 		limit: NUM_POINTS,
 	    aggregate: amount, //amount of days/hours/minutes in between data points
 		toTs: endTimeStamp //last unix timestamp included
 	}
-	
+
 	$.ajax({
-	    type: "GET", 
+	    type: "GET",
 	    url: url,
 		data: params,
-	    success : handleData 
+	    success : handleData
 	});
 }
 
 function handleData(recv) {
 	if (recv.Response==="Error") {
 		throw "Error: "+recv.Message
-		
+
 	} else if (recv.Response==="Success") {
 		data=recv.Data;
 		data.forEach(function(d) { d.time = new Date(d.time * 1000); });
 		console.log(data);
-		
+
 		updateGraphs();
-		
+
 	} else {
 		throw "Unknown response message: "+recv.Response
 	}
 }
 
 function setUp() {
-<<<<<<< HEAD
-	graph=d3.select("#graph1")
-		.append("svg")
-		.attr("width", width)
-		.attr("height", height);
-		
-	graph.append("text")
-		.attr("y", padding.top - 5)
-		.text("scroll up")
-		.style("cursor", "hand")
-		.on("click", scrollUp);
-		
-	graph.append("text")
-		.attr("y", padding.top - 5)
-		.attr("x", 70)
-		.text("scroll down")
-		.style("cursor", "hand")
-		.on("click", scrollDown);
-		
-	graph.append("text")
-		.attr("y", padding.top - 5)
-		.attr("x", 200)
-		.text("drag left")
-		.style("cursor", "hand")
-		.on("click", dragLeft);
-		
-	graph.append("text")
-		.attr("y", padding.top - 5)
-		.attr("x", 270)
-		.text("drag right")
-		.style("cursor", "hand")
-		.on("click", dragRight);
-
-	xScale = d3.scaleTime()                      
-					.range([padding.left, width - padding.right]);
-	  
-	yScale = d3.scaleLinear()
-					.range([height - padding.bottom, padding.top]);
-						
-	xAxis = d3.axisBottom() 
-					.scale(xScale)
-					.tickFormat(timeFormat);
-						
-	yAxis = d3.axisLeft()
-					.scale(yScale);
-					
-	graph.append("g") 
-		.attr("class", "x axis")
-		.attr("transform", `translate(0, ${height - padding.bottom})`)
-		.call(xAxis)
-		.selectAll("text")	    // rotate the axis labels
-			.style("text-anchor", "end")
-			.attr("dx", "-.8em")
-			.attr("dy", ".15em")
-			.attr("transform", "rotate(-30)");
-	  
-	graph.append("g") 
-		.attr("class", "y axis")
-		.attr("transform", `translate(${padding.left}, 0)`)
-		.call(yAxis);
-}
-
-function scrollUp() {
-	NUM_POINTS=Math.max(10,Math.ceil(NUM_POINTS/1.4)); //make sure theres enough points remaining
-	var dist=(end-start)/4;
-	
-	start=new Date(start.getTime()+dist);
-	end=new Date(end.getTime()-dist);
-	
-	requestData();
-}
-
-function scrollDown() {
-	NUM_POINTS=Math.min(100,Math.floor(NUM_POINTS*1.4)); //limit the amount of points
-	var dist=(end-start)/2;
-	
-	start=new Date(Math.max(start.getTime()-dist,minimumDate));
-	end=new Date(Math.min(end.getTime()+dist,(new Date()).getTime()));
-	
-	requestData();
-}
-
-function dragRight(dist=86400000) { //in dit geval gaan we terug in de tijd
-	dist=Math.min(dist,start.getTime()-minimumDate);
-	
-	start=new Date(start.getTime()-dist);
-	end=new Date(end.getTime()-dist);
-	
-	requestData();
-}
-
-function dragLeft(dist=86400000) { // nu gaan we vooruit in de tijd
-	dist=Math.min(dist,(new Date()).getTime()-end.getTime());
-	
-	start=new Date(start.getTime()+dist);
-	end=new Date(end.getTime()+dist);
-	
-	requestData();
-=======
 	if (graphType==="candle") {
 		setUpCandleChart();
 	} else if (graphType==="donut") {
-		//functie voor setup van donut
+		setUpDonutChart()
 	} else if (graphType==="line") {
 		setUpLineChart();
 	} else {
 		throw "Invalid graph type.";
-	} 
->>>>>>> 8a6cda8216eb8ffc4a1d2bd6d31ab310d9ed22fc
+	}
 }
 
 function updateGraphs() {
 	if (graphType==="candle") {
 		drawCandleChart();
 	} else if (graphType==="donut") {
-		//functie om donut te tekenen
+		drawDonutChart([]);
 	} else if (graphType==="line") {
 		drawLineChart();
 	} else {
 		throw "Invalid graph type.";
-	} 
+	}
 }
